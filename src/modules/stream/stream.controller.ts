a660fc360d99de3ffa0b47105d4f8b56920fc217
konovalov <konovalov@elma-bpm.com>
2023-06-23 18:19:00 +0400

    test streaming

--- src/modules/stream/stream.controller.ts
+++ src/modules/stream/stream.controller.ts
@@ -1,9 +1,105 @@
-import {Router} from "express";
+import {Router, Response, Request, NextFunction} from 'express'
+import WebTorrent, {Torrent, TorrentFile} from 'webtorrent'
 
 const router = Router()
-router.get('/', (req, res) => {
-    res.status(200).send({
-        hello: 'world'
+const client = new WebTorrent()
+
+let state = {
+    progress: 0, downloadSpeed: 0, ratio: 0
+}
+
+let error;
+
+client.on('error', (err: Error) => {
+    console.error('err', err.message)
+    error = err.message
+})
+
+client.on('torrent', () => {
+    console.log(client.progress)
+    state = {
+        progress: Math.round(client.progress * 100 * 100) / 100,
+        downloadSpeed: client.downloadSpeed,
+        ratio: client.ratio
+    }
+})
+
+router.get('/add/:magnet', (req: Request, res: Response) => {
+    const magnet = req.params.magnet
+
+    client.add(magnet, (torrent) => {
+        const files = torrent.files.map(el => ({name: el.name, length: el.length}))
+        res.status(200).send(files)
+    })
+})
+router.get('/stats', (req: Request, res: Response) => {
+    state = {
+        progress: Math.round(client.progress * 100 * 100) / 100,
+        downloadSpeed: client.downloadSpeed,
+        ratio: client.ratio
+    }
+    res.status(200).send(state)
+})
+
+// stream
+interface StreamRequest extends Request {
+    params: {
+        magnet: string,
+        fileName: string
+    },
+    headers: {
+        range: string
+    }
+}
+
+interface ErrorWithStatus extends Error {
+    status: number
+}
+
+router.get('/:magnet/:fileName', (req: StreamRequest, res: Response, next: NextFunction) => {
+    const {
+        params: {magnet, fileName}, headers: {
+            range
+        }
+    } = req
+    if (!range) {
+        const error = <ErrorWithStatus>new Error('Range is not defined, please make request from HTML5 Player')
+        error.status = 416
+        return next(error)
+    }
+    // file searching
+    const torrentFile = client.get(magnet) as Torrent
+    let file = <TorrentFile>{}
+
+    for (let i = 0; i < torrentFile.files.length; i++) {
+        const currentTorrentPiece = torrentFile.files[i]
+        if (currentTorrentPiece.name === fileName) {
+            file = currentTorrentPiece
+        }
+    }
+    const fileSize = file.length
+    const [startParsed, endParsed] = range.replace(/bytes=/, '').split('-')
+
+    const start = Number(startParsed)
+    const end = endParsed ? Number(endParsed) : fileSize - 1
+
+    const chunkSize = end - start + 1
+    const headers = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunkSize,
+        'Content-Type': 'video/mp4'
+    }
+    res.writeHead(206, headers)
+
+    const streamPosition = {
+        start,
+        end
+    }
+    const stream = file.createReadStream(streamPosition)
+    stream.pipe(res)
+    stream.on('error', err => {
+        return next(err)
     })
 })
 
